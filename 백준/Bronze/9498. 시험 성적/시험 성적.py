ecdfc1785a97e514a0be26b61b88b5d034debc91
ChoiMinKyu <154953644+takealittletime@users.noreply.github.com>
2024-09-06 17:21:36 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,16 @@
+record = int(input().strip())
+
+if (100>=record and record>=90):
+    print('A')
+
+elif (record >= 80):
+    print('B')
+
+elif (record >= 70):
+    print('C')
+
+elif (record >= 60):
+    print('D')
+
+else:
+    print('F')
