85acf5304923d8573b97b5a39fced44a64033f36
ChoiMinKyu <154953644+takealittletime@users.noreply.github.com>
2024-09-23 10:59:35 +0900

    [Gold III] Title: 아침 산책, Time: 844 ms, Memory: 103644 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,62 +1,44 @@
-# 21606 아침산책
 import sys
-input = sys.stdin.readline
-# print = sys.stdout.write
 sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 
-# N, A 입력
-N = int(input())
+n = int(input())
+
 A = input().rstrip()
 
-# 실내/실외 노드 여부 할당할 리스트
-is_indoor = [0] * (N+1)
+graph = [[] for _ in range(n+1)]
+place = [0] * (n+1)
+visited = [0] * (n+1)
+
 for i in range(len(A)):
     if A[i] == '1':
-        is_indoor[i+1] = 1
+        place[i+1] = 1
 
-# 그래프 입력
-graph = [[] for _ in range(N+1)]
-for i in range(N-1):
-    u,v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
-
-# 경로 결과, 방문 여부 할당할 리스트
-result = [0] * (N+1)
-visited = [0] * (N+1)
-
-# 실외 노드에 대한 반복을 수행하는 dfs 함수
-def dfs(neighbor, node):
-    # (기저 조건) 인접 노드(neighbor)가 실내라면 경로 결과 추가하고 리턴
-    if is_indoor[neighbor] == 1:
-        result[neighbor] += 1
-        result[node] += 1
-        return
+for _ in range(n-1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
     
-    # 인접 노드가 실외라면
+def dfs(node):
+    res = 0 
+    for next_node in graph[node]:
+        if place[next_node] == 0:
+            if not visited[next_node]:
+                visited[next_node] = 1
+                res += dfs(next_node)
+        else:
+            res += 1
+    return res
+
+ans = 0
+for i in range(1, n+1):
+    if place[i] == 0:
+        if not visited[i]:
+            visited[i] = 1
+            res = dfs(i)
+            ans += res * (res - 1)
     else:
-        for next_neighbor in graph[neighbor]:
-            if visited[next_neighbor] == 0:
-                visited[next_neighbor] = 1
-                dfs(next_neighbor, node)
-                visited[next_neighbor] = 0 #실외 노드는 방문값 다시 0으로
-
-for i in range(1,N+1):
-    # 실내 노드 선택
-    if is_indoor[i] == 1:
-        visited[i] = 1 # 방문 표시
-        # 인접한 노드들에 대해
-        for neighbor in graph[i]:
-            # 인접 노드가 실내 노드일 때
-            if is_indoor[neighbor] == 1:
-                # 아직 방문하지 않은 노드라면 경로 결과에 추가
-                if visited[neighbor] == 0:
-                    result[neighbor] += 1
-                    result[i] += 1
-            # 인접 노드가 실외 노드일 때
-            else:
-                visited[neighbor] = 1 #방문 표시
-                dfs(neighbor, i)
-                visited[neighbor] = 0 #실외 노드는 방문값 다시 0으로
-
-print(sum(result))
+        for next_node in graph[i]:
+            if place[next_node] == 1:
+                ans += 1
+print(ans)
