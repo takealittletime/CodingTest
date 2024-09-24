52bd94fac35921c18e421f8fbd72984bd6c68712
ChoiMinKyu <154953644+takealittletime@users.noreply.github.com>
2024-09-24 13:17:24 +0900

    [Gold III] Title: 줄 세우기, Time: 164 ms, Memory: 38408 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,41 +1,35 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
-# 노드의 개수와 간선의 개수 입력
-v, e = map(int, input().split())
-# 모든 노드에 대한 진입차수는 0으로 초기화
-indegree = [0] * (v + 1)
-# 각 노드에 연결된 간선 정보를 담기 위한 연결 리스트
-graph = [[] for _ in range(v + 1)]
 
-for _ in range(e):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    indegree[b] += 1
+N, M = map(int,input().split())
+graph = [[] for _ in range(N+1)]
+indegree = [0] * (N+1)
 
+for _ in range(M):
+    A, B = map(int,input().split())
+    graph[A].append(B)
+    indegree[B] += 1
 
-# 위상 정렬 함수
-def topology_sort():
+def topology_sort(N):
     result = []
     q = deque()
 
-    for i in range(1, v + 1):
+    # 진입차수가 0인 노드들을 모두 큐에 할당
+    for i in range(1, N+1):
         if indegree[i] == 0:
             q.append(i)
-
+    
     while q:
-        now = q.popleft()
-        result.append(now)
-        # 해당 원소와 연결된 노드들의 진입차수에서 1빼기
-        for g in graph[now]:
-            indegree[g] -= 1
-            if indegree[g] == 0:
-                q.append(g)
+        current = q.popleft()
+        result.append(current)
 
-    # 위상 정렬 수행한 결과 출력
+        for neighbor in graph[current]:
+            indegree[neighbor] -= 1
+            if indegree[neighbor] == 0:
+                q.append(neighbor)
+        
     for res in result:
         print(res, end=' ')
 
-
-topology_sort()
+topology_sort(N)
