48a814ed8a1f5d66a0fb9be7e4901c702598021b
ChoiMinKyu <154953644+takealittletime@users.noreply.github.com>
2024-09-26 01:42:41 +0900

    [Silver II] Title: DFS와 BFS, Time: 60 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,61 +1,44 @@
-# 시스템 입/출력에 sys.stdin/stdout 사용
 import sys
+from collections import deque
 input = sys.stdin.readline
-print = sys.stdout.write
 
-# 깊이 우선 탐색
 def dfs(V):
-    # 전역변수 N, visited1 이용
-    global N
-    global visited1
-    
-    # 노드 V 출력 후 방문 처리.
-    print(str(V) +' ')
+    print(V, end = ' ')
     visited1[V] = 1
 
-    # 노드 V와 인접한 미방문 노드에 대해 재귀적으로 과정 반복.
-    for i in range(1, N+1):
-        if (graph[V][i] == 1) and (visited1[i] == 0):
-            dfs(i)
+    for neighbor in sorted(graph[V]):
+        if not visited1[neighbor]:
+            dfs(neighbor)
 
-# 너비 우선 탐색
 def bfs(V):
-    
-    # 전역변수 N, visited2 이용
-    global N
-    global visited2
 
-    # 노드 V를 큐에 삽입 후 방문 처리.
-    queue = [V]  
+    q = deque([V])
     visited2[V] = 1
 
-    # 큐에 값이 있다면 루프 반복
-    while queue:
-        # 방문한 노드를 큐에서 꺼내 제거.
-        V = queue.pop(0)
-        print(str(V) +' ')
+    while q:
+        current = q.popleft()
+        print(current, end=' ')
+        
 
-        # 노드 V와 인접한 미방문 노드를 큐에 삽입 후 방문 처리.
-        for i in range(1, N+1):
-            if (visited2[i] == 0 and graph[V][i] == 1):
-                queue.append(i)
-                visited2[i] = 1
+        for neighbor in sorted(graph[current]):
+            if not visited2[neighbor]:
+                q.append(neighbor)
+                visited2[neighbor] = 1
 
-# 정점의 개수 N, 간선의 개수 M, 시작 노드 V 입력
-N, M, V = map(int, input().split())
 
-# 인접행렬 방식으로 그래프 표현
-graph = [ [0]*(N+1) for _ in range(N+1) ]
-# 그래프 입력
+
+N, M, V = map(int,input().split())
+graph = [[] for _ in range(N+1)]
 for i in range(M):
-    a, b = map(int, input().split())
-    graph[a][b] = graph[b][a] = 1
+    u,v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
-# 방문 노드를 할당할 visited 리스트
 visited1 = [0] * (N+1)
-visited2 = visited1.copy()
+visited2 = [0] * (N+1)
 
-# DFS, BFS 각각 실행 및 출력
 dfs(V)
-print('\n')
+print()
 bfs(V)
+
+# print(graph)
