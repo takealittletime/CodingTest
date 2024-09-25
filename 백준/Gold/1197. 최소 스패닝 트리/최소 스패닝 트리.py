0bc7cac4aa390e4e461370e6160e01ce95f00fb8
ChoiMinKyu <154953644+takealittletime@users.noreply.github.com>
2024-09-25 20:56:36 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 200 ms, Memory: 49216 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,60 +1,49 @@
-# def find_recur(parent,x):
-#     if x != parent[x]:
-#         find_recur(parent,parent[x])
-#     return parent[x]
-
 import sys
 input = sys.stdin.readline
 
 def find(parent, x):
     while parent[x] != x:
-        # 경로 압축: 부모를 할당
         parent[x] = parent[parent[x]]
         x = parent[x]
-    return x    
+    return x
 
-def union(parent, rank, x, y):
-    rootX = find(parent,x)
-    rootY = find(parent,y)
+def union(parent,rank,x,y):
+    rootX = find(parent, x)
+    rootY = find(parent, y)
 
     if rootX != rootY:
         if rank[rootX] > rank[rootY]:
-            parent[rootY] = rootX
-        elif rank[rootX] < rank[rootY]:
-            parent[rootX] = rootY
+            parent[rootY] = parent[rootX]
+        elif rank[rootY] > rank[rootX]:
+            parent[rootX] = parent[rootY]
         else:
-            parent[rootY] = rootX
+            parent[rootY] = parent[rootX]
             rank[rootX] += 1
 
 def kruskal(V, edges):
-
-    # 부모 노드
-    parent = [i for i in range(V+1)]
-    # 트리 높이
-    rank = [0] * (V+1)
-    # 가중치를 기준으로 간선 오름차순 정렬
-    edges.sort(key=lambda x:x[2])
     mst_weight = 0
     mst_edges = 0
 
+    parent = [i for i in range(V+1)]
+    rank = [0]*(V+1)
+    edges.sort(key=lambda x:x[2])
+
     for u,v,w in edges:
-        if find(parent, u) != find(parent, v):
-            union(parent, rank, u, v)
+
+        if find(parent,u) != find(parent,v):
+            union(parent,rank,u,v)
             mst_weight += w
             mst_edges += 1
-
-            if mst_edges == V - 1:
+            if mst_edges == V-1:
                 break
-    
+
     return mst_weight
 
-
-
-V, E = map(int, input().split())
+V, E = map(int,input().split())
 edges = []
-
 for i in range(E):
-    A,B,C = map(int,input().split())
+    A, B, C = map(int,input().split())
     edges.append((A,B,C))
 
-print(kruskal(V,edges))
+result = kruskal(V, edges)
+print(result)
