456b992bb949c50399757e541eb68888cad8fe0d
ChoiMinKyu <154953644+takealittletime@users.noreply.github.com>
2024-09-27 21:30:44 +0900

    [Silver III] Title: 01타일, Time: 116 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,11 +1,13 @@
+# 입력에 stdin 사용
 import sys
 input = sys.stdin.readline
 
-def tile(n):
-    if (n == 1):
-        return 1
-    if (n==2):
-        return 2
+def dp(n):
+    # n = 1 또는 2 일 때 그대로 출력
+    if (n == 1) or (n == 2):
+        return n
+    
+    # curr은 2에서, pre는 1에서 시작하며 각각 이전 값들을 저장하면서 수행
     curr = 2
     pre = 1
     for i in range(3,n+1):
@@ -14,5 +16,6 @@ def tile(n):
         curr = next
     return curr
 
+# n 입력, 결과 출력
 n = int(input())
-print(tile(n))
+print(dp(n))
