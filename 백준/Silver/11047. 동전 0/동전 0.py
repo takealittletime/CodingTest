2c681b3aae899907eff0dfe20460436c44d7d373
ChoiMinKyu <154953644+takealittletime@users.noreply.github.com>
2024-09-29 00:53:34 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -3,18 +3,14 @@ input = sys.stdin.readline
 
 N, K = map(int, input().split())
 A = []
+
 for _ in range(N):
     A.append(int(input()))
+A.sort(reverse=True)
 
-A.sort(reverse=True)  # 내림차순으로 정렬
-
-count = 0  # 초기 동전 개수를 0으로 설정
+count = 0
 for coin in A:
-    if K == 0:
-        break
-    
-    # 현재 동전 가치로 필요한 동전의 개수 추가
-    count += K // coin
-    K %= coin  # 나머지 금액 갱신
+    count += K//coin
+    K = K%coin
 
 print(count)
