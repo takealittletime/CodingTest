29d7d31bf4a7038fcc7dcf19e1a838cabd439c4c
ChoiMinKyu <154953644+takealittletime@users.noreply.github.com>
2024-10-17 15:07:12 +0900

    [Gold III] Title: 크게 만들기, Time: 156 ms, Memory: 38836 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin.readline
+
+n,k = map(int, input().split())
+numbers = list(input())
+
+answer = []
+cnt = k
+for num in numbers:
+    while answer and cnt>0 and answer[-1] < num:
+        del answer[-1]
+        cnt -= 1
+    answer.append(num)
+
+print(''.join(answer[:n-k]))
