d28ee1874375f4e7c0f1d689eb5a2f2a70c1eacb
ChoiMinKyu <154953644+takealittletime@users.noreply.github.com>
2024-10-17 22:09:05 +0900

    [Gold V] Title: 용액, Time: 100 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+solutions = list(map(int,input().split()))
+
+left = 0
+right = N-1
+
+best_mix = sys.maxsize
+best_left = 0
+best_right = 0
+
+while (left < right):
+    mix = solutions[left] + solutions[right]
+    if (abs(mix) < abs(best_mix)):
+        best_mix = mix
+        best_left = left
+        best_right = right
+    
+    if mix < 0:
+        left += 1
+    else:
+        right -= 1
+
+print(solutions[best_left],solutions[best_right])
