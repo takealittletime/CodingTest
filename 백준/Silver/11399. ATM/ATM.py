79fc6f1cb59cb1f7a8e4192b79a5f7f74bd441bc
ChoiMinKyu <154953644+takealittletime@users.noreply.github.com>
2024-10-19 15:32:55 +0900

    [Silver IV] Title: ATM, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+P = list(map(int,input().split()))
+P.sort()
+
+answer = 0
+
+for i in range(N):
+    for j in range(0,i+1):
+        answer += P[j]
+
+print(answer)
