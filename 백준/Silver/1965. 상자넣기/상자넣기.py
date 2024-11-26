7998ec036bfbde999e388df8afcb7edecb85deee
ChoiMinKyu <154953644+takealittletime@users.noreply.github.com>
2024-11-26 18:03:38 +0900

    [Silver II] Title: 상자넣기, Time: 200 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+boxes = list(map(int,input().split()))
+dp = [1] * n
+
+for i in range(1,n):
+  for j in range(i):
+    if boxes[j] < boxes[i]:
+      dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))
