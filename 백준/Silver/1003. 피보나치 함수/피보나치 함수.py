6e3a491fa8ffe1cfd40a901b9380577a8781ff24
ChoiMinKyu <154953644+takealittletime@users.noreply.github.com>
2025-01-31 21:17:42 +0900

    [Silver III] Title: 피보나치 함수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+  N = int(input())
+  zero, one = 1,0
+
+  for i in range(N):
+    zero,one = one, zero+one
+  
+  print(zero,one)
